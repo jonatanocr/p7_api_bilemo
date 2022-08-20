528dd997973b2e75ffe84b212cfd1b5bbd85241a
jonatanocr <jmbzk@pm.me>
2022-08-20 06:47:22 +0200

    add entity Product

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        for ($i=0; $i<10; $i++) {
+            $product = new Product();
+        }
+        $manager->flush();
+    }
+}
