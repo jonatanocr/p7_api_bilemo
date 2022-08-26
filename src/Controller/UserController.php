7a02979af7cb16a2218ea493fce397ad5965ca9e
jonatanocr <jmbzk@pm.me>
2022-08-26 05:37:13 +0200

    add createUser and updateUser root

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,13 +3,18 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Repository\CustomerRepository;
 use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
+
 
 class UserController extends AbstractController
 {
@@ -36,4 +41,38 @@ class UserController extends AbstractController
         $em->flush();
         return new JsonResponse(null, Response::HTTP_NO_CONTENT);
     }
+
+    #[Route('/api/users', name: 'createUser', methods: ['POST'])]
+    public function createUser(Request $request, SerializerInterface $serializer, EntityManagerInterface $em,
+      UrlGeneratorInterface $urlGenerator, CustomerRepository $customerRepository)
+    {
+        $user = $serializer->deserialize($request->getContent(), User::class, 'json');
+        $content = $request->toArray();
+        $idCustomer = $content['idCustomer'] ?? -1;
+        $user->setCustomer($customerRepository->find($idCustomer));
+
+        $em->persist($user);
+        $em->flush();
+
+        $jsonUser = $serializer->serialize($user, 'json', ['groups' => 'getUsers']);
+        $location = $urlGenerator->generate('detailUser', ['id' => $user->getId()], UrlGeneratorInterface::ABSOLUTE_URL);
+        return new JsonResponse($jsonUser, Response::HTTP_CREATED, ["Location" => $location], true);
+    }
+
+    #[Route('/api/users/{id}', name:"updateUser", methods:['PUT'])]
+    public function updateUser(Request $request, SerializerInterface $serializer,
+      User $currentUser, EntityManagerInterface $em, CustomerRepository $customerRepository)
+    {
+        $updatedUser = $serializer->deserialize($request->getContent(),
+                User::class,
+                'json',
+                [AbstractNormalizer::OBJECT_TO_POPULATE => $currentUser]);
+        $content = $request->toArray();
+        $idCustomer = $content['idCustomer'] ?? -1;
+        $updatedUser->setCustomer($customerRepository->find($idCustomer));
+
+        $em->persist($updatedUser);
+        $em->flush();
+        return new JsonResponse(null, JsonResponse::HTTP_NO_CONTENT);
+   }
 }
