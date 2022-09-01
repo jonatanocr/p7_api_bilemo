a8c1386074fbac761c92aeba1d269ad1c42057eb
jonatanocr <jmbzk@pm.me>
2022-09-01 05:42:26 +0200

    create root create delete update product

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -4,11 +4,16 @@ namespace App\Controller;
 
 use App\Entity\Product;
 use App\Repository\ProductRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
 use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class ProductController extends AbstractController
 {
@@ -27,5 +32,53 @@ class ProductController extends AbstractController
         $jsonProduct = $serializer->serialize($product, 'json');
         return new JsonResponse($jsonProduct, Response::HTTP_OK, [], true);
     }
+
+    #[Route('/api/products/{id}', name: 'deleteProduct', methods: ['DELETE'])]
+    public function deleteProduct(Product $product, EntityManagerInterface $em)
+    {
+        $em->remove($product);
+        $em->flush();
+        return new JsonResponse(null, Response::HTTP_NO_CONTENT);
+    }
+
+    #[Route('/api/products', name: 'createProduct', methods: ['POST'])]
+    public function createProduct(Request $request, SerializerInterface $serializer, EntityManagerInterface $em,
+      UrlGeneratorInterface $urlGenerator, ValidatorInterface $validator)
+    {
+        $product = $serializer->deserialize($request->getContent(), Product::class, 'json');
+        $content = $request->toArray();
+
+        $errors = $validator->validate($product);
+        if ($errors->count() > 0) {
+          $messages = [];
+           foreach ($errors as $error) {
+                $messages[] = $error->getMessage();
+           }
+
+          return new JsonResponse($serializer->serialize($messages, 'json'), JsonResponse::HTTP_BAD_REQUEST);
+        }
+
+        $em->persist($product);
+        $em->flush();
+
+        $jsonProduct = $serializer->serialize($product, 'json');
+        $location = $urlGenerator->generate('detailProduct', ['id' => $product->getId()], UrlGeneratorInterface::ABSOLUTE_URL);
+        return new JsonResponse($jsonProduct, Response::HTTP_CREATED, ["Location" => $location], true);
+    }
+
+    #[Route('/api/products/{id}', name:"updateProduct", methods:['PUT'])]
+    public function updateUser(Request $request, SerializerInterface $serializer,
+      Product $currentProduct, EntityManagerInterface $em)
+    {
+        $updatedProduct = $serializer->deserialize($request->getContent(),
+                Product::class,
+                'json',
+                [AbstractNormalizer::OBJECT_TO_POPULATE => $currentProduct]);
+        $content = $request->toArray();
+
+        $em->persist($updatedProduct);
+        $em->flush();
+        return new JsonResponse(null, JsonResponse::HTTP_NO_CONTENT);
+   }
 
 }
